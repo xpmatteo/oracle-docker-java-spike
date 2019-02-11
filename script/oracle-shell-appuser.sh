f9b6a1a6e74d629b40abd86fef268dae346999ef
Matteo Vaccari <mvaccari@thoughtworks.com>
2019-02-11 12:14:21 +0100

    Add support for login.sql autoload in sqlplus

--- script/oracle-shell-appuser.sh
+++ script/oracle-shell-appuser.sh
@@ -4,5 +4,8 @@ set -e
 cd $(dirname $0)/..
 source script/lib/common.sh
 
-exec docker exec -it $ORACLE_CONTAINER_NAME bash -c "source /home/oracle/.bashrc; sqlplus appuser/appuser@ORCLPDB1"
+# ORACLE_PATH is defined so that sqlplus will autoload login.sql in the db
+# folder, which is mounted in the container under /tmp/outside
+exec docker exec -it $ORACLE_CONTAINER_NAME \
+  bash -c "source /home/oracle/.bashrc; export ORACLE_PATH=/tmp/outside/db; sqlplus appuser/appuser@ORCLPDB1"
  
